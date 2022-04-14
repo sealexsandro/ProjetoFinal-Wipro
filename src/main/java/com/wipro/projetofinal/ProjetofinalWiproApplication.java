9fb08880389156d2111ff6b30f4edccb66e499d8
Murilo F. Saidel <51511259+muriloFsaidel@users.noreply.github.com>
2022-04-14 20:39:00 -0300

    menu interativo

--- src/main/java/com/wipro/projetofinal/ProjetofinalWiproApplication.java
+++ src/main/java/com/wipro/projetofinal/ProjetofinalWiproApplication.java
@@ -9,22 +9,23 @@ import com.wipro.projetofinal.service.ManageAccounts;
 
 @SpringBootApplication
 public class ProjetofinalWiproApplication {
-
-	public static void main(String[] args) {
-		// Não apaguem esta linha de baixo
-//		SpringApplication.run(ProjetofinalWiproApplication.class, args);
-
+	
+	public static void implementacaoManual() {
+		/*
 		CheckingAccount contaCorrente1 = new CheckingAccount(0.0, "Mario", "529.982.247-25");
 		CheckingAccount contaCorrente2 = new CheckingAccount(0.0, "Joao", "529.982.247-27");
 		CheckingAccount contaCorrente3 = new CheckingAccount(0.0, "Ana Beatriz", "529.982.247-28");
 		//	CheckingAccount contaCorrente3 = new CheckingAccount(0.0, "Maria", "529.982.247-25");
 		//	CheckingAccount contaCorrente4 = new CheckingAccount(0.0, "Neves", "529.982.247-25");
+		
+		SpecialAccount contaEspecial1 = new SpecialAccount(0.0,"Freddy",300.00,"529.982.247-29");
 
 		ManageAccounts gerenciaContas = new ManageAccounts();
 
 		gerenciaContas.addAccount(contaCorrente1);
 		gerenciaContas.addAccount(contaCorrente2);
 		gerenciaContas.addAccount(contaCorrente3);
+		gerenciaContas.addAccount(contaEspecial1);
 
 		// Imprimir todas as contas
 		gerenciaContas.showAllAccounts();
@@ -44,6 +45,174 @@ public class ProjetofinalWiproApplication {
 		gerenciaContas.activateCreditCard(contaCorrente1);
 
 		System.out.println("Depois de adicionar creditCard: " + contaCorrente1.getCreditCard());
+		
+		
+		/*teste conta especial
+		contaEspecial1.deposit(200);
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		
+		contaEspecial1.withdraw(100);
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		
+		contaEspecial1.withdraw(100);
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		
+		contaEspecial1.withdraw(100);
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		
+		contaEspecial1.withdraw(200);
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		
+		contaEspecial1.withdraw(15);
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		*/
+		
+		/*teste conta corrente
+		contaCorrente1.deposit(200);
+		System.out.println(gerenciaContas.showAccount("529.982.247-25"));
+		
+		contaCorrente1.withdraw(100);		
+		System.out.println(gerenciaContas.showAccount("529.982.247-25"));
+		
+		contaCorrente1.withdraw(100);		
+		System.out.println(gerenciaContas.showAccount("529.982.247-25"));
+		
+		contaCorrente1.withdraw(100);		
+		System.out.println(gerenciaContas.showAccount("529.982.247-25"));
+		*/
+		
+		/*try {
+		    contaCorrente1.deposit(-1);
+		    
+			contaCorrente1.withdraw(10);
+		}catch(ErrorTypes e) {
+			System.err.println(e.getMessage());
+		}
+		System.out.println(gerenciaContas.showAccount("529.982.247-25"));
+		*/
+		
+		/* saques
+		contaCorrente1.withdraw(10);
+		
+		contaEspecial1.withdraw(300);
+		
+		System.out.println(gerenciaContas.showAccount("529.982.247-29"));
+		
+		contaEspecial1.withdraw(10);
+		
+		*/
+	}
+
+	
+	public static void showMenu() {
+
+		System.out.println("************************************************");
+		System.out.println("\t\t Wipro Banking\n \t\t Menu de opções \t\t\t");
+		System.out.println("************************************************");
+		System.out.println("[1] Criar conta");
+		System.out.println("[2] Consultar uma conta");
+		System.out.println("[3] Consultar todas as contas");
+		System.out.println("[4] Sair");
+		System.out.println("************************************************");
+		System.out.println("Escolha uma opção:");
+	}
+
+	public static void main(String[] args) {
+		// Não apaguem esta linha de baixo
+//		SpringApplication.run(ProjetofinalWiproApplication.class, args);
+
+		
+		ManageAccounts ma = new ManageAccounts();
+		
+		Scanner leia = new Scanner(System.in);
+		String nome,cpf;
+		int op,tipoConta;
+		char decision = ' ';
+		
+		do {
+			
+			showMenu();			
+			op = leia.nextInt();
+			
+			switch(op) {
+				
+			case 1:
+				System.out.println("***************************************");
+				System.out.println("Escolha o tipo de conta: [1] Conta corrente [2] Conta especial");
+				tipoConta = leia.nextInt();
+				
+				
+				while(tipoConta != 1 && tipoConta != 2) {
+					System.out.println("Escolha o tipo de conta: [1] Conta corrente [2] Conta especial");
+					tipoConta = leia.nextInt();
+				}		
+				
+				System.out.println("Digite o nome do cliente:");
+				leia.nextLine();
+				nome = leia.nextLine();				
+				System.out.println("Digite o cpf do cliente:");
+				cpf = leia.next();
+				
+				while(!ValidationAccount.isCPF(cpf)) {					
+					System.out.println("O mer irmão, presta atenção! Digite um cpf válido:");
+					cpf = leia.next();
+					//666.88.996-7-false
+					//888.888.888-67-true
+				}				
+				
+				if(tipoConta == 1) {					
+					CheckingAccount cc = new CheckingAccount(0.0,nome,cpf);
+					
+					System.out.println("\n"+cc.toString()+"\n");
+					
+				
+					System.out.println("Confirma os dados? [S][s]sim [N][n]não");
+					
+					decision =  leia.next().charAt(0);
+					
+					if(decision == 'S' || decision == 's') {
+						ma.addAccount(cc);					
+						System.out.println("Conta corrente adicionada  com sucesso");
+					}else {
+						System.out.println("Conta não criada");
+						cc = null;
+					}	
+					
+				}else if(tipoConta == 2) {
+					SpecialAccount sa = new SpecialAccount(0.0,nome,300.0,cpf);
+					System.out.println("\n"+sa.toString()+"\n");
+					
+					System.out.println("Confirma os dados? [S][s]sim [N][n]não");
+					decision =  leia.next().charAt(0);
+					if(decision == 'S' || decision == 's') {
+						ma.addAccount(sa);					
+						System.out.println("Conta especial adicionada com sucesso");
+					}else {
+						System.out.println("Conta não criada");
+						sa = null;
+					}	
+				}				
+				break;
+				
+			case 2:
+				System.out.println("Digite o cpf do cliente:");
+				cpf = leia.next();
+				System.out.println(""+ma.showAccount(cpf)+"");
+				break;
+				
+			case 3:
+				ma.showAllAccounts();
+				break;	
+				
+			case 4:	
+				break;
+				
+			default:
+				System.err.println("Escolha uma opção seu jeca!");
+				break;
+			}			
+			
+		}while(op != 4);
 
 	}
 
